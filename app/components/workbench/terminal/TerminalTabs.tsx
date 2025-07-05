3a73b608f5362ac51ef7f0558e7128991f17872a
blacknova <markuenomasahiro@gmail.com>
2025-07-05 10:15:03 +0800

    changed Low Impact - Ui Text and docummentation

--- app/components/workbench/terminal/TerminalTabs.tsx
+++ app/components/workbench/terminal/TerminalTabs.tsx
@@ -107,7 +107,7 @@ export const TerminalTabs = memo(() => {
                       onClick={() => setActiveTerminal(index)}
                     >
                       <div className="i-ph:terminal-window-duotone text-lg" />
-                      Bolt Terminal
+                      Prompify Terminal
                     </button>
                   ) : (
                     <React.Fragment>
